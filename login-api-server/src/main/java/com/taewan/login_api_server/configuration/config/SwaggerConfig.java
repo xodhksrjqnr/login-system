884f5f08a77d05fdc5ad6858b86a258cc534bb8c
xodhksrjqnr <xodhksrjqnr@naver.com>
2024-06-18 23:33:29 +0900

    파일 이동

--- login-api-server/src/main/java/com/taewan/login_api_server/configuration/config/SwaggerConfig.java
+++ login-api-server/src/main/java/com/taewan/login_api_server/configuration/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.taewan.login_api_server.configuration.others;
+package com.taewan.login_api_server.configuration.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
